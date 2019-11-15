4c5caa0c87603bc35327b331dca818e52b5d1020
Miles Leska <37006041+ArmoredGit@users.noreply.github.com>
2019-11-15 15:12:21 -0600

    editing on PC to fix

--- projects/starfieldJS.js
+++ projects/starfieldJS.js
@@ -1,4 +1,4 @@
-  var jeff;
+var jeff;
 var gloabalNewA;
 var gloabalNewV;
 var gloabalNewT;
@@ -50,3 +50,100 @@ function draw() {
     gloabalNewT = random(0, 360);
     gloabalNewR = random(0, 255);
     gloabalNewG = random(0, 255);
+    gloabalNewB = random(0, 255);
+    if (gloabalTic > 300) {
+      gloabalTic = 0;
+      gloabalRun++;
+      if (gloabalRun > 4) {
+        gloabalRun = 0;
+      }
+    }
+  }
+  for (let i = 0; i < jeff.length; i++) {
+    if (dist(jeff[i].getx(), jeff[i].gety(), width / 2, height / 2) > width || jeff[i].getAge() > 500) {
+      jeff.splice(i, 1);
+    }
+  }
+}
+
+class NormalParticle {
+  //your code here
+  constructor() {
+    this.x = width / 2;
+    this.y = height / 2;
+    this.speed = 2;
+    this.tic = 0;
+    this.a = gloabalNewA;
+    this.v = gloabalNewV;
+    this.r = gloabalNewR + random(-20, 20);
+    this.g = gloabalNewG + random(-20, 20);
+    this.b = gloabalNewB + random(-20, 20);
+    if (gloabalRun == 0) {
+      this.angle = ((random([0, 1, ]) == 0) ? random(180, 240) : random(0, 60)) + gloabalNewT;
+    } else if (gloabalRun == 1) {
+      this.angle = random(0, 60) + gloabalNewT;
+      this.speed = 6;
+    } else if (gloabalRun == 2) {
+      this.angle = random(0, 360) + gloabalNewT;
+    } else if (gloabalRun == 3) {
+      this.angle = random(0, 60) + gloabalNewT;
+    } else if (gloabalRun == 4) {
+      this.angle = gloabalNewT;
+      this.x = random(0, width);
+      this.y = random(0, height);
+    }
+  }
+
+  move() {
+    this.tic++;
+    this.x += this.speed * cos(radians(this.angle));
+    this.y += this.speed * sin(radians(this.angle));
+    this.angle -= ((this.a * this.tic) + this.v);
+  }
+
+  show() {
+    fill(this.r, this.g, this.b);
+    ellipse(this.x, this.y, 10, 10);
+  }
+
+  getx() {
+    return this.x;
+  }
+
+  gety() {
+    return this.y;
+  }
+
+  getAge() {
+    return this.tic;
+  }
+}
+
+class OddballParticle extends NormalParticle //uses an varerface
+{
+  constructor() {
+    super();
+  }
+
+  move() {
+    super.r = gloabalNewR + random(-20, 20);
+    super.g = gloabalNewG + random(-20, 20);
+    super.b = gloabalNewB + random(-20, 20);
+  }
+
+  show() {
+    ellipse((width / 2) + random(-4, 4), (height / 2) + random(-4, 4), 10, 10);
+  }
+
+  getx() {
+    return (width / 2);
+  }
+
+  gety() {
+    return (height / 2);
+  }
+
+  getAge() {
+    return 0;
+  }
+}
