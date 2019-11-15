e0e57ae48254729f11d2aa46ea198904ada82633
Miles Leska <37006041+ArmoredGit@users.noreply.github.com>
2019-11-15 11:43:35 -0600

    Fixed mistakes typing in code

--- projects/starfieldJS.js
+++ projects/starfieldJS.js
@@ -54,79 +54,6 @@ function draw(){
     if(gloabalTic > 300){
       gloabalTic = 0;
       gloabalRun++;
-    }
-  }
-  for(let i = 0; i < jeff.length; i++){
-    if(dist(jeff[i].getx(),jeff[i].gety(),width/2,height/2) > width || jeff[i].getAge() > 500){
-      jeff.splice(i,1);
-    }
-  }
-}
-
-class NormalParticle{
-//your code here
-  constructor(){
-    this.x = width/2;
-    this.y = height/2;
-    this.speed = 2;
-    this.angle = ((random([0,1,]) == 0)?random(180,240):random(0,60)) + gloabalNewT;
-    this.tic = 0;
-var jeff;
-var gloabalNewA;
-var gloabalNewV;
-var gloabalNewT;
-var gloabalNewR;
-var gloabalNewG;
-var gloabalNewB;
-var gloabalTic;
-var gloabalRun;
-
-function setup(){
-  //your code here
-  createCanvas(window.innerWidth, window.innerHeight);
-  noStroke();
-  fill(255);
-  gloabalNewA = -0.01;
-  gloabalNewV = random(2,5);
-  gloabalNewT = random(0,360);
-  gloabalNewR = random(0,255);
-  gloabalNewG = random(0,255);
-  gloabalNewB = random(0,255);
-  gloabalTic = 0;
-  gloabalRun = 0;
-  jeff = [];
-  for(let i = 0; i < 10; i++){
-    jeff.push(new OddballParticle());
-  }
-  for(let i = 0; i < 50; i++){
-    jeff.push(new NormalParticle());
-  }
-}
-
-function draw(){
-  //your code here
-  gloabalTic ++;
-  background(0);
-  for(let i = 0; i < jeff.length; i++){
-    jeff[i].move();
-  }
-  for(let i = 0; i < jeff.length; i++){
-    jeff[i].show();
-  }
-  if(gloabalTic < 100){
-    for(let i = 0; i < 50; i++){
-      jeff.push(new NormalParticle());
-    }
-  } else {
-    gloabalNewA = -0.01;
-    gloabalNewV = random(2,5);
-    gloabalNewT = random(0,360);
-    gloabalNewR = random(0,255);
-    gloabalNewG = random(0,255);
-    gloabalNewB = random(0,255);
-    if(gloabalTic > 300){
-      gloabalTic = 0;
-      gloabalRun++;
      if(gloabalRun > 4){
          gloabalRun = 0;
       }
@@ -219,4 +146,3 @@ class OddballParticle extends NormalParticle//uses an varerface
     return 0;
   }
 }
-
